eda2352aad17a300e557d0eb04630af1bb711c90
meamruri <meammf@yandex.ru>
2018-04-19 18:58:45 +0300

    add record receiver

---
+++ src/main/kotlin/gateway/Receiver.kt
@@ -0,0 +1,23 @@
+package gateway
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.BodyHandler
+
+class Receiver: AbstractVerticle() {
+    private val eventBus = vertx.eventBus()
+
+    override fun start() {
+        val router = Router.router(vertx)
+        router.route().handler(BodyHandler.create())
+        router.post("/").handler(this::handleNewRecord)
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080)
+    }
+
+    private fun handleNewRecord(routingContext: RoutingContext) {
+        val body = routingContext.bodyAsJson
+        eventBus.publish("records-feed", body)
+    }
+
+}
