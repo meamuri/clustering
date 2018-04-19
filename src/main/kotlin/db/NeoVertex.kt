863358d2767786dec9b8dce620c82cad021e0afe
meamruri <meammf@yandex.ru>
2018-04-19 19:38:38 +0300

    add neo4j connector

---
+++ src/main/kotlin/db/NeoVertex.kt
@@ -0,0 +1,41 @@
+package db
+
+import io.vertx.core.AbstractVerticle
+import org.neo4j.driver.v1.AuthTokens
+import org.neo4j.driver.v1.GraphDatabase
+import org.neo4j.driver.v1.Values
+import java.time.Instant
+
+private const val CONN_ADDR = "bolt://localhost:7687"
+private const val USER = "diplom-admin"
+private const val PASS = "admin"
+
+class NeoVertex: AbstractVerticle() {
+    private val addNodeQuery = "CREATE (a:\$tid {body: \$body, ts: \$ts})"
+
+    private val driver = GraphDatabase.driver(CONN_ADDR, AuthTokens.basic(USER, PASS))
+    override fun start() {
+
+    }
+
+    override fun stop() {
+        driver.closeAsync()
+    }
+
+    private fun writeNode(tid: Int, body: String, ts: Instant) {
+        val session = driver.session()
+
+        session.use {
+             session.writeTransaction { tx ->
+                val result = tx.run(addNodeQuery,
+                        Values.parameters(
+                                "tid", tid,
+                                "body", body,
+                                "ts", ts
+                        ))
+                result.single().get( 0 ).asString()
+            }
+        } // .. session use
+    } // fun writeNode
+
+}
