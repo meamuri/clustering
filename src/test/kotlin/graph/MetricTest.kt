b84e94584d9494987db7b9989fb0bb12f31eaa3e
meamruri <meammf@yandex.ru>
2018-04-20 16:42:46 +0300

    more metric tests

--- src/test/kotlin/graph/MetricTest.kt
+++ src/test/kotlin/graph/MetricTest.kt
@@ -23,4 +23,26 @@ class MetricTest {
 
         assertTrue { abs(metric - 1) < eps }
     }
+
+    @Test
+    fun `graphs without equals nodes have zero metric value`() {
+        val graph = Graph(recordGenerator(NEO4J_MATCH))
+        val otherGraph = Graph(recordGenerator(NEO4J_QUERY))
+
+        val metric = getMetrics(graph, otherGraph)
+        assertTrue { abs(metric) < eps }
+    }
+
+    @Test
+    fun `half similar nodes should be equal to 50% metric value`() {
+        val graph = Graph(recordGenerator(NEO4J_MATCH))
+        val otherGraph = Graph(recordGenerator(NEO4J_QUERY))
+
+        graph.registerRecord(recordGenerator(NEO4J_RELATION))
+        otherGraph.registerRecord(recordGenerator(NEO4J_RELATION))
+
+        val metric = getMetrics(graph, otherGraph)
+        assertTrue { abs(metric) - 0.5 < eps }
+    }
+
 }
