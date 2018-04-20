3f9a1e14b27163741bd3127f0260080dd5fb6f3a
meamruri <meammf@yandex.ru>
2018-04-20 12:50:41 +0300

    red test for future work

--- src/test/kotlin/graph/ManagerTest.kt
+++ src/test/kotlin/graph/ManagerTest.kt
@@ -8,16 +8,27 @@ import kotlin.test.assertEquals
 class ManagerTest {
 
     lateinit var recordGenerator: (Int, String) -> Record
+    lateinit var manager: Manager
 
     @Before
     fun setUp() {
         recordGenerator = getTidSpecificGenerator()
+        manager = Manager()
     }
 
     @Test
     fun `first record should create new graph`() {
-        val m = Manager()
-        m.registerRecord(recordGenerator(1, NEO4J_RELATION))
-        assertEquals(0, m.currentExecutor)
+        manager.registerRecord(recordGenerator(1, NEO4J_RELATION))
+        assertEquals(0, manager.currentExecutor)
     }
+
+    @Test
+    fun `record with different TIDs should be processed in different graphs`() {
+        manager.registerRecord(recordGenerator(1, NEO4J_RELATION))
+        manager.registerRecord(recordGenerator(2, NEO4J_RELATION))
+        assertEquals(0, manager.findTidProcessor(1))
+        assertEquals(1, manager.findTidProcessor(2))
+        assertEquals(1, manager.currentExecutor)
+    }
+
 }
